1ad25e18be3f80a7976dedbdcd050779f901d9cb
Ansh Aggarwal <anshaggarwal490@gmail.com>
2023-06-24 23:27:48 +0530

    First Commit

---
+++ server/lib/dbConnection.js
@@ -0,0 +1,18 @@
+const {MongoClient} = require('mongodb')
+//const url='mongodb://127.0.0.1:27017/'
+const url = 'mongodb+srv://anshaggarwal490:ansh123@quizapp.vlbn2oi.mongodb.net/'
+const client = new MongoClient(url);
+
+const dbName = 'quizApp';
+
+async function dbconnect (collectionName) {
+
+    // Setting up the connection
+
+    const result = await client.connect();
+    const db = result.db(dbName);
+    return db.collection(collectionName);
+
+}
+
+module.exports = dbconnect;
