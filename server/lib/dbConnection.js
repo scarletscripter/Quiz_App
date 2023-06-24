0204e23ff95b9a2b68f0ec6dd4ee05a36e878b00
Ansh Aggarwal <anshaggarwal490@gmail.com>
2023-06-24 23:43:03 +0530

    Update the database url

--- server/lib/dbConnection.js
+++ server/lib/dbConnection.js
@@ -1,6 +1,5 @@
 const {MongoClient} = require('mongodb')
-//const url='mongodb://127.0.0.1:27017/'
-const url = 'mongodb+srv://anshaggarwal490:ansh123@quizapp.vlbn2oi.mongodb.net/'
+const url='mongodb://127.0.0.1:27017/'
 const client = new MongoClient(url);
 
 const dbName = 'quizApp';
